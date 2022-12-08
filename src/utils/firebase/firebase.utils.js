1e3b2e13f6caf3fe00b39b56660cb266b4edd61d
hayleyesu <haylajul03@gmail.com>
2022-12-08 23:02:25 +0300

    fetching data from firestore db functionality added

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,16 @@ import {
   signOut,
   onAuthStateChanged
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { 
+  getFirestore, 
+  doc, 
+  getDoc, 
+  setDoc,
+  collection,
+  writeBatch ,
+  query,
+  getDocs
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCBajD8lVqE3zJ9DJ6-Efg7kz0WIgFpVMw",
@@ -34,7 +43,41 @@ export const signInWithGooglePopup = () =>
 export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
-export const db = getFirestore();
+  export const db = getFirestore();
+
+  //To upload data to our firestore database.
+
+  export const addCollectionAndDocuments=async (collectionKey,objectsToAdd)=>{
+    const collectionRef=collection(db,collectionKey);
+    const batch=writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+
+      const docRef=doc(collectionRef,object.title.toLowerCase());
+      batch.set(docRef,object);
+      
+    });
+    await batch.commit();
+    console.log('done')
+  } 
+   //For retrive data from firebase database
+  
+  export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'catagories');
+    const q = query(collectionRef);
+  
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+      const { title, items } = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  
+    return categoryMap;
+  };
+  
+
+
 
 export const createUserDocumentFromAuth = async (
   userAuth,
