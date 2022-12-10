03bd727b0d3971d0504334f0be5e8215eeeb597a
hayleyesu <haylajul03@gmail.com>
2022-12-10 16:36:04 +0300

    adding catagory componet to filter the catagories inside the shop component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -18,7 +18,7 @@ const CheckoutItem=({cartItem})=>{
         <span className='quantity'>
             <div className='arrow' onClick={removeItemHandler}>&#10094;</div>
             <span className='value'>{quantity}</span>
-            <div className='arrow' onClick={addItemHandler}> &#10095;</div>
+            <div className='arrow' onClick={addItemHandler }> &#10095;</div>
             </span>
            <span className='price'>{price}</span>
         
--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -2,28 +2,29 @@ import {createContext,useState,useEffect} from 'react';
 
 import { getCategoriesAndDocuments  } from '../utils/firebase/firebase.utils.js';
 
-export const ProductsContext= createContext(
+export const CategoriesContext= createContext(
     {
-products:[],
+categoriesMap:{},
 
 })
 
-export const ProductsProvider=({children})=>{
+export const CategoriesProvider=({children})=>{
 
-    const [products,setProducts]=useState([]);
+    const [categoriesMap,setCategoriesMap]=useState({});
 
     
     useEffect(()=>{
         const getCategoriesMap=async()=>{
             const categoryMap= await getCategoriesAndDocuments();
             console.log(categoryMap);
+            setCategoriesMap(categoryMap);
         }
         getCategoriesMap();
         
         },[]);
 
-    const value={products};
-return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+    const value={categoriesMap};
+return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
 }
 
 
